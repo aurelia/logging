af35d70fea9bbabe7098809ad8c4d1c1b2268a48
Rob Eisenberg <rob@bluespire.com>
2015-01-15 01:54:42 -0500

    doc(all): add missing docs and tags

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@
 
  /**
  * Enum specifying the levels of the logger
-* 
-* @property levels
-* @type {Enum}
-*/
+ * 
+ * @property levels
+ * @type {Enum}
+ * @for export
+ */
 export var levels = {
   none: 0,
   error:1,
@@ -89,13 +90,23 @@ function createLogger(id){
 
 ﻿/**
  * Gets an instance of a logger by the Id used when creating.
- * Returns the instance of the logger, or creates a new logger if none exists for that Id.
- * @param {int} id - The id of the logger you wish to get an instance of.
+ *
+ * @method getLogger
+ * @param {string} id - The id of the logger you wish to get an instance of.
+ * @return {Logger} The instance of the logger, or creates a new logger if none exists for that Id.
+ * @for export
  */
 export function getLogger(id){
   return loggers[id] || (loggers[id] = createLogger(id));
 }
 
+/**
+ * Adds an appender capable of processing logs and channeling them to an output.
+ *
+ * @method addAppender
+ * @param {object} appender - An appender instance to begin processing logs with.
+ * @for export
+ */
 export function addAppender(appender){
   appenders.push(appender);
 
@@ -108,14 +119,16 @@ export function addAppender(appender){
 
 ﻿/**
  * Sets the level of the logging for the application loggers
+ *
+ * @method setLevel
  * @param {int} level - matches an enum specifying the level of logging.
+ * @for export
  */
 export function setLevel(level){
   logLevel = level;
 }
 
 /**
-* The logging module encapsulates all logic related to logging.
 * The logger is essentially responsible for having log statements that appear during debugging but are squelched
 * when using the build tools, depending on the log level that is set.  The available levels are -
 * 1. none
@@ -123,7 +136,8 @@ export function setLevel(level){
 * 3. warn
 * 4. info
 * 5. debug
-* you cannot instantiate the logger directly - you must use the getLogger method instead.*
+*
+* You cannot instantiate the logger directly - you must use the getLogger method instead.
 *
 * @class Logger
 * @constructor
@@ -137,8 +151,35 @@ export class Logger {
     this.id = id;
   }
 
+  /**
+   * Logs a debug message.
+   *
+   * @method debug
+   * @param {string} message - the message to log
+   */
   debug(){}
+
+  /**
+   * Logs info.
+   *
+   * @method info
+   * @param {string} message - the message to log
+   */
   info(){}
+
+  /**
+   * Logs a warning.
+   *
+   * @method warn
+   * @param {string} message - the message to log
+   */
   warn(){}
+
+  /**
+   * Logs an error.
+   *
+   * @method error
+   * @param {string} message - the message to log
+   */
   error(){}
 }
