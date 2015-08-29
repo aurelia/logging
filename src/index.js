ce4b66bd92a170112cd443def9c5b7a6da9fe2b5
Rob Eisenberg <rob@bluespire.com>
2015-08-28 22:26:29 -0400

    chore(index): remove some old unneeded docs comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-/**
- * This library is part of the Aurelia platform and contains a minimal but effective logging mechanism
- * with support for log levels and pluggable log appenders.
- *
- * @module logging
- */
-
  /**
  * Creates an instance of Error that aggregates and preserves an innerError.
  */
