dcb1c5d674e62959910855dfae520f85101488de
Rob Eisenberg <rob@bluespire.com>
2015-11-15 23:43:57 -0500

    chore(all): prepare release 1.0.0-beta.1

--- dist/es6/aurelia-logging.d.ts
+++ dist/es6/aurelia-logging.d.ts
@@ -35,9 +35,37 @@ declare module 'aurelia-logging' {
   * Implemented by classes which wish to append log data to a target data store.
   */
   export interface Appender {
+    
+    /**
+      * Appends a debug log.
+      *
+      * @param logger The source logger.
+      * @param rest The data to log.
+      */
     debug(logger: Logger, ...rest: any[]): void;
+    
+    /**
+      * Appends an info log.
+      *
+      * @param logger The source logger.
+      * @param rest The data to log.
+      */
     info(logger: Logger, ...rest: any[]): void;
+    
+    /**
+      * Appends a warning log.
+      *
+      * @param logger The source logger.
+      * @param rest The data to log.
+      */
     warn(logger: Logger, ...rest: any[]): void;
+    
+    /**
+      * Appends an error log.
+      *
+      * @param logger The source logger.
+      * @param rest The data to log.
+      */
     error(logger: Logger, ...rest: any[]): void;
   }
   
